6676fb79d20b4f40e37cdeb5b2d664e30ba07550
DuongAty <atyduongtran@gmail.com>
2026-01-21 10:38:12 +0700

    Setup GitHub Actions deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './page/Dashboard';
